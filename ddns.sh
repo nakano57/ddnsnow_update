2540a53c1d0b78162878cfaa8c3847defc412552
nakano <hiroto856@gmail.com>
2019-10-05 23:31:11 +0900

    fix

--- ddns.sh
+++ ddns.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo -n `date +"[%m-%d %H:%M:%S] "` >> ~/DDNSNow_update.log 
-echo -n "　" >> ~/DDNSNow_update.log 
+echo -n " x" >> ~/DDNSNow_update.log 
 wget -O- "https://f5.si/update.php?domain=<domain>&password=<pass>" >> ~/DDNSNow_update.log
 echo -n "\n" >> ~/DDNSNow_update.log 
